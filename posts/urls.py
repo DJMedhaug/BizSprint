266b650d395c55c23226746145cf064e5ee41663
danamedhaug <djmedhaug@gmail.com>
2016-07-22 15:19:02 -0700

    added delete and edit to blog

--- posts/urls.py
+++ posts/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 	url(r'^$', post_list, name='list'),
     url(r'^create/$', post_create, name='create'),
     url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
-    url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='update'),
-    url(r'^(?P<slug>[\w-]+)/delete/$', post_delete),
+    url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='edit'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', post_delete, name='delete'),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
